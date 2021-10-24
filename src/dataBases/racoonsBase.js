7d9bc5ae019dd2c8103b76a8483025a4beee51a6
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-10-24 15:39:01 +0300

    Initial commit

---
+++ src/dataBases/racoonsBase.js
@@ -0,0 +1,22 @@
+export default [
+    {
+name: "Poloskun",
+location: "Australia",
+abilities: ['stealing', 'washing', 'being cute']
+},
+{
+    name: "Gray",
+    location: "Australia",
+    abilities: ['stealing', 'washing', 'being cute']
+},
+{
+    name: "John",
+    location: "Australia",
+    abilities: ['stealing', 'murdering', 'being cute']
+    },
+    {
+        name: "Vick",
+        location: "Australia",
+        abilities: ['stealing', 'washing', 'being cute']
+        }
+]

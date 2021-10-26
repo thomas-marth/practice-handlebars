6de527bf5d484ba84014de6bfd3a278d62b6a6d3
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-10-26 15:05:42 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./css/style.css";
+import ditto from "./dataBases/ditto.json";
+import bulbasaur from "./dataBases/bulbasaur.json";
+import pokemonTemplate from "./templates/pokemonTemplate.hbs";
+
+// console.log(ditto);
+// console.log(pokemonTemplate(ditto));
+
+function markupMaker(...params) {
+  params.map((param) => {
+    pokemonTemplate(param);
+    document.body.insertAdjacentHTML("beforeend", pokemonTemplate(param));
+  });
+}
+
+markupMaker(ditto, bulbasaur);
